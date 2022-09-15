718f38ecdc66a926c64a60b20d626d66356ffb64
CIT173F-20@M307.UC-BCF.EDU.PH <CIT173F-20@M307.UC-BCF.EDU.PH>
2022-09-15 10:57:28 +0800

    phpAct

---
+++ ventura.php
@@ -0,0 +1,54 @@
+<?php 
+echo "Exercise 1 \n"; 
+echo "Twinkle, Twinkle little star.";
+$txt1 = "Twinkle";
+$txt = "star";
+
+echo " $txt1, $txt1 little $txt.\n";
+?>
+
+<?php
+echo "Exercise 2 \n";
+ $x=10; 
+ $y=7;
+
+
+$result = $x + $y ;
+ echo "$x + $y = " . ($x + $y) ."\n";
+ echo "$x - $y = " . ($x - $y) ."\n";
+ echo "$x * $y = " . ($x * $y) ."\n";
+ echo "$x / $y = " . ($x / $y) ."\n";
+ echo "$x % $y = " . ($x % $y) ."\n";
+?>
+
+<?php
+echo "Exercsie 3 \n";
+ $x=8; 
+
+ echo "Value is now " . $x . "\n";
+ $x += 2;
+ echo "Value is now " . $x . "\n";
+ $x -= 4;
+ echo "Value is now " . $x . "\n";
+ $x *= 5;
+ echo "Value is now " . $x . "\n";
+ $x /= 3;
+ echo "Value is now " . $x  . "\n";
+ $x = ++$x;
+ echo "Value is now " . $x  . "\n";
+ $x = --$x;
+ echo "Value is now " . $x  . "\n";
+ ?>
+ 
+<?php
+echo "Exercise 4 \n";
+$name = "Harry";
+$age = 28;
+var_dump($name); "\n";
+print_r ($name); "\n";
+var_dump($age);
+
+$name = null;
+var_dump($name);
+
+ ?>
